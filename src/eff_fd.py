5ab730bb881789645aee5f1a2088b6654980f159
Hugo Aboud <hugoaboud@gmail.com>
2023-02-01 05:51:31 -0300

    performance refactor on EffFD

--- src/eff_fd.py
+++ src/eff_fd.py
@@ -19,18 +19,23 @@ class EffFD:
         w = img.shape[1]
         y = int((h-M)/2)
         x = int((w-M)/2)
-        return img[y:y+M,x:x+M], M
+        return img[y:y+M,x:x+M]
 
     @staticmethod
-    def _getMass(img, M, s):
-        mass = 0
-        for y in range(0,img.shape[0],s):
-            for x in range(0,img.shape[1],s):
-                box = img[y:y+s,x:x+s]
-                min = np.min(box)
-                max = np.max(box)
-                mass += max-min+1
-        return mass
+    def _resize(img):
+        M = 2 ** int(math.log(img.shape[0], 2))
+        return cv2.resize(img, (M,M))
+
+    @staticmethod
+    def _getMass(img, s):
+        height, width = img.shape[:2]
+        cell_height = height // s
+        cell_width = width // s
+        reshaped_image = img.reshape(s, cell_height, s, cell_width).swapaxes(1,2)
+        reshaped_image = reshaped_image.astype(np.uint16)
+        grid_max = np.amax(reshaped_image, axis=(2, 3))
+        grid_min = np.amin(reshaped_image, axis=(2, 3))
+        return np.sum(grid_max - grid_min + 1)
 
     @staticmethod
     def _fit(masses, plot=False):
@@ -47,16 +52,21 @@ class EffFD:
         return fit[0]
 
     @staticmethod
-    def getHausdorffDimension(img):
-        # plt.imshow(img, cmap='magma')
-        # plt.show()
-        img, M = SDBC._crop(img)
+    def getHausdorffDimension(img, plot=False):
+        img = EffFD._crop(img)
+        img = EffFD._resize(img)
+
+        if (plot):
+            plt.imshow(img, cmap='magma')
+            plt.show()
+
         masses = []
-        s = int(M/2)
-        while (s > 1):
-            masses += [(M/s,SDBC._getMass(img, M, s))]
-            s = int(s/2)
-        # S = range(2, int(M/2)+1, 1)
-        # masses = [(M/s,SDBC._getMass(img, M, s)) for s in S]
-        dim = SDBC._fit(masses)
+        M = img.shape[0]
+        s = 2
+        
+        while (s < M):
+            masses += [(s,EffFD._getMass(img, s))]
+            s *= 2
+        
+        dim = EffFD._fit(masses, plot)
         return dim
